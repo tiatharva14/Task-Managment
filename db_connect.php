36e104cc16779785c8c8066b06a607b3c9e34414
tiatharva14 <155054897+tiatharva14@users.noreply.github.com>
2024-03-01 14:40:07 +0530

    update

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','tms_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('mysqlazsver01.mysql.database.azure.com','myadmin@mysqlazsver01','Server@1','tms_db1')or die("Could not connect to mysql".mysqli_error($con));
